723f889453656849b5daf43732dac2ba06a1aaca
Charlie Cheever <ccheever@gmail.com>
2015-09-23 17:58:31 -0700

    Using `fs.access` since `fs.exists` is deprecated
    Sometimes you actually do want to know if a file exists!!!

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ var fs = require('fs');
 
 module.exports = function (pth) {
   return new Promise(function (resolve, reject) {
-    fs.exists(pth, function (exists) {
-      resolve(exists);
+    fs.access(pth, fs.F_OK, function (err, result) {
+      if (err) {
+        if (err.toString().match(/^Error: ENOENT:/)) {
+          resolve(false);
+        } else {
+          reject(err);
+        }
+      } else {
+        resolve(true);
+      }
     });
   });
 }
